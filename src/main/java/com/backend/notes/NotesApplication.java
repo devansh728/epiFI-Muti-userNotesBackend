14188561b2d96ae5814341a4422fcfe7f084e89b
devansh728 <gargdevansh728@gmail.com>
2026-05-17 00:33:45 +0530

    fix: forget pushing NotesApplication updation

--- src/main/java/com/backend/notes/NotesApplication.java
+++ src/main/java/com/backend/notes/NotesApplication.java
@@ -1,18 +1,15 @@
 package com.backend.notes;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableCaching
-@EnableJpaAuditing
 @OpenAPIDefinition(
 	info = @Info(
 		title = "Notes App API",
